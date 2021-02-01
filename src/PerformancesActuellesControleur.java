48335d697dc4c472e1bc76043ea2f73c391d20c9
Bounter <theomureddu@gmail.com>
2021-02-01 08:41:48 +0100

    Ajout des variables de session

--- src/PerformancesActuellesControleur.java
+++ src/PerformancesActuellesControleur.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class PerformancesActuellesControleur
@@ -35,6 +36,9 @@ public class PerformancesActuellesControleur extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		HttpSession session = request.getSession();
+		UtilisateurBeanModele utilisateur = (UtilisateurBeanModele) session.getAttribute("utilisateur");		
+		request.setAttribute("utilisateur", utilisateur);
 	}
 
 }
