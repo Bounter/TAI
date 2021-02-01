cac98498085fa671388fbf3db9c8dc3a9cb769b1
Bounter <theomureddu@gmail.com>
2021-02-01 08:39:38 +0100

    Revert "Ajout des variables session dans le controleur"
    This reverts commit ba6ff9b8ea842c7475255854d4f07368196624f2.

--- src/PerformancesActuellesControleur.java
+++ src/PerformancesActuellesControleur.java
@@ -6,7 +6,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class PerformancesActuellesControleur
@@ -14,14 +13,14 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/PerformancesActuellesControleur")
 public class PerformancesActuellesControleur extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public PerformancesActuellesControleur() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PerformancesActuellesControleur() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -36,9 +35,6 @@ public class PerformancesActuellesControleur extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-		HttpSession session = request.getSession();
-		UtilisateurBeanModele utilisateur = (UtilisateurBeanModele) session.getAttribute("utilisateur");		
-		request.setAttribute("utilisateur", utilisateur);
 	}
 
 }
